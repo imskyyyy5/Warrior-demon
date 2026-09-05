7fad75b97c84b639ea9806af0a22b8ee7202cd1a
Sameer Pandey <sushmapandey135@gmail.com>
2026-09-05 14:22:28 +0545

    ...

--- warrior-demon/Global.gd
+++ warrior-demon/Global.gd
@@ -1,4 +1,8 @@
-extends Button
+extends Node
+
+
+var minigames_done = 0 #track how many minigames done
+var lives = 5 #track how many lives left, also effecting garlic appearing
 
 
 # Called when the node enters the scene tree for the first time.
